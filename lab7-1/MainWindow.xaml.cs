c635600ec53a76f46531bb12cae672d406762642
Daniel RY <daniil_ry@hotmail.com>
2022-04-20 13:02:00 +0200

    Добавьте файлы проекта.

---
+++ lab7-1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab7_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<string> list;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            list = new List<string>();      //Выделяем память под лист
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (InputText.Text.Length!=0)
+            {
+                list.Add(InputText.Text);
+                Spisok.Items.Add(InputText.Text);
+                InputText.Clear();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int max = 0;
+            foreach (string s in list)
+            {
+                if (s.Length > max) max = s.Length;
+            }
+            Result.Text = max.ToString();
+        }
+    }
+}
